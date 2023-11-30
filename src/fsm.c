945543da500c72dac1a55927f864b46e7aedb48b
Nicholas H.R. Sims <nickhrsims@gmail.com>
2023-11-30 18:12:48 +0000

    fix: heavily repair broken semantics of matrix types

--- src/fsm.c
+++ src/fsm.c
@@ -3,18 +3,21 @@
 
 #include "fsm.h"
 
-fsm_state_id_t fsm_get_target(fsm_state_id_t **fsm, fsm_trigger_id_t trigger,
-                              fsm_state_id_t current_state) {
+fsm_state_id_t fsm_get_target(size_t state_count, size_t trigger_count,
+                              fsm_state_id_t fsm[state_count][trigger_count],
+                              fsm_trigger_id_t trigger, fsm_state_id_t current_state) {
     assert(fsm);
     assert(fsm[current_state]);
     return fsm[current_state][trigger];
 }
 
-void fsm_send(fsm_state_id_t **fsm, fsm_trigger_id_t trigger,
+void fsm_send(size_t state_count, size_t trigger_count,
+              fsm_state_id_t fsm[state_count][trigger_count], fsm_trigger_id_t trigger,
               fsm_state_id_t *current_state) {
     assert(fsm);
     assert(fsm[current_state]);
-    fsm_state_id_t next_state = fsm_get_target(fsm, trigger, *current_state);
+    fsm_state_id_t next_state =
+        fsm_get_target(state_count, trigger_count, fsm, trigger, *current_state);
     if (next_state) {
         *current_state = next_state;
     }
--- src/fsm.h
+++ src/fsm.h
@@ -5,7 +5,10 @@
 typedef uint8_t fsm_state_id_t;
 typedef uint8_t fsm_trigger_id_t;
 
-fsm_state_id_t fsm_get_target(fsm_state_id_t **fsm, fsm_trigger_id_t trigger,
+fsm_state_id_t fsm_get_target(size_t state_count, size_t trigger_count,
+                              fsm_state_id_t fsm[state_count][trigger_count],
+                              fsm_trigger_id_t trigger,
                               fsm_state_id_t current_state);
-void fsm_send(fsm_state_id_t **fsm, fsm_trigger_id_t trigger,
-              fsm_state_id_t *current_state);
+void fsm_send(size_t state_count, size_t trigger_count,
+              fsm_state_id_t fsm[state_count][trigger_count],
+              fsm_trigger_id_t trigger, fsm_state_id_t *current_state);
