4d8133b3e9c95e1f37a346a7016a01d6f815c4db
Nicholas H.R. Sims <nickhrsims@gmail.com>
2023-11-30 17:57:40 +0000

    refactor: cleanup fsm types

--- src/fsm.c
+++ src/fsm.c
@@ -3,18 +3,19 @@
 
 #include "fsm.h"
 
-fsm_state_id_t fsm_get_target(fsm_state_table_t fsm, fsm_trigger_id_t trigger,
+fsm_state_id_t fsm_get_target(fsm_state_id_t **fsm, fsm_trigger_id_t trigger,
                               fsm_state_id_t current_state) {
-  assert(fsm);
-  assert(fsm[current_state]);
-  return fsm[current_state][trigger];
+    assert(fsm);
+    assert(fsm[current_state]);
+    return fsm[current_state][trigger];
 }
 
-void fsm_send(fsm_state_table_t fsm, fsm_trigger_id_t trigger,
+void fsm_send(fsm_state_id_t **fsm, fsm_trigger_id_t trigger,
               fsm_state_id_t *current_state) {
-  assert(fsm);
-  assert(fsm[current_state]);
-  fsm_state_id_t next_state = fsm[*current_state][trigger];
-  if (next_state)
-    *current_state = next_state;
+    assert(fsm);
+    assert(fsm[current_state]);
+    fsm_state_id_t next_state = fsm_get_target(fsm, trigger, *current_state);
+    if (next_state) {
+        *current_state = next_state;
+    }
 }
